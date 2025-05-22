b7dfce1669d1046fab4d7f8759982c73d169da12
Sri vaishnave <kurapativaishnave@gmail.com>
2025-05-22 20:34:47 +0530

    Class_Obj_Method_signatures_done

--- Java_Basics_1.java
+++ Java_Basics_1.java
@@ -1,4 +1,21 @@
 //How to create a class, object, method and its signature 
 public class Java_Basics_1 {
-    
+   static class Person {
+        String name;
+        int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+        public void greet() {
+            System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+        }
+    }
+    public static void main(String[] args) {
+        Person person1 = new Person("vaish", 20);
+        person1.greet();
+    }
 }
+
+
